1ecd54cb00cd6940c779f67fe6699b626ded3012
Greelan <53196309+Greelan@users.noreply.github.com>
2025-09-09 22:18:47 +1000

    Update aloftwinescript.sh

--- aloftwinescript.sh
+++ aloftwinescript.sh
@@ -19,15 +19,34 @@ export WINEDEBUG=fixme-all
 export DISPLAY=:$DPY_NUM
 export XDG_RUNTIME_DIR=/tmp
 
-wget -q -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-chmod +x winetricks
-wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/9.1.0/wine-mono-9.1.0-x86.msi
+# Get Wine major version
+WINE_VERSION=$(/usr/bin/wine --version | grep -oP '\d+' | head -1)
 
-/usr/bin/wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
+# Determine correct Wine Mono version
+case "$WINE_VERSION" in
+  8) MONO_VERSION="8.1.0" ;;
+  9) MONO_VERSION="9.4.0" ;;
+  10) MONO_VERSION="10.1.0" ;;
+  *)
+    echo "Unsupported Wine version: $WINE_VERSION"
+    kill $XVFB_PID
+    exit 1
+    ;;
+esac
+
+# Setup winetricks and install Mono
+[[ -f winetricks ]] && rm -f winetricks
+wget -q https://raw.githubusercontent.com/Winetricks/winetricks/refs/tags/20250102/src/winetricks
+chmod +x winetricks
+
+MONO_URL="https://dl.winehq.org/wine/wine-mono/${MONO_VERSION}/wine-mono-${MONO_VERSION}-x86.msi"
+wget -q -O "$WINEPREFIX/mono.msi" "$MONO_URL"
+/usr/bin/wine msiexec /i "$WINEPREFIX/mono.msi" /qn /quiet /norestart /log "$WINEPREFIX/mono_install.log"
 
 PACKAGES="vcrun2022 wmp11 sound=disabled"
 echo "" > winescript_log.txt 2>&1
 for PACKAGE in $PACKAGES; do
+  if [[ "$PACKAGE" == "vcrun2022" && "$WINE_VERSION" == "10" ]]; then continue; fi
   ./winetricks -q $PACKAGE >> winescript_log.txt 2>&1
 done
 rm -rf ~/.cache/winetricks
